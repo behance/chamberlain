665645e932f5cb4ef4335ad9818a75cfd82df116
chr0n1x <heilong24@gmail.com>
2014-10-24 17:57:53 -0400

    chamberlain/cli/main: lightly wrapping jenkins-jobs cli options

--- chamberlain/cli/main.py
+++ chamberlain/cli/main.py
@@ -1,11 +1,37 @@
 #!/usr/bin/env python
 
 import chamberlain
+import logging
+import os
 import sys
 
+from jenkins_jobs import cmd as jenkins_cmd
 
-def main():
-    print "ohai"
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+
+def create_parser():
+    parser = jenkins_cmd.create_parser()
+    return parser
+
+
+def main(argv=None):
+    parser = create_parser()
+    options = parser.parse_args(argv)
+
+    if not options.command:
+        parser.error("Must specify a 'command' to be performed")
+    if (options.log_level is not None):
+        options.log_level = getattr(logging,
+                                    options.log_level.upper(),
+                                    logger.getEffectiveLevel())
+        logger.setLevel(options.log_level)
+
+    options.setdefault('conf',
+                       os.path.join(os.getcwd(),
+                       '.jenkins-job-builder.ini'))
+    print options
 
 
 def version():
