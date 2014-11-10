3ef4d0bc38c7acf87e91c1b163c7891bf47e0da2
chr0n1x <heilong24@gmail.com>
2014-11-10 10:35:12 -0500

    splitting out config tests

--- tests/chamberlain/application_test.py
+++ tests/chamberlain/application_test.py
@@ -1,7 +1,7 @@
 import chamberlain.application as chap
-
 import unittest
 
+from chamberlain.config import NullConfig
 from mock import patch
 
 
@@ -11,20 +11,8 @@ class AppHomeTest(unittest.TestCase):
             self.assertEquals(chap.app_home(), "user_home/.chamberlain")
 
 
-class NullConfigTest(unittest.TestCase):
-    def getattr_returns_self_test(self):
-        cfg = chap.NullConfig()
-        self.assertEqual(cfg, cfg.nested_thing)
-
-    def call_returns_none_test(self):
-        self.assertEqual(chap.NullConfig().thing(), None)
-
-    def nothing_exists_test(self):
-        self.assertEqual(chap.NullConfig().thing.exists(), False)
-
-
 class GithubClientTest(unittest.TestCase):
     def repo_memory_list_test(self):
-        client = chap.GithubClient(chap.NullConfig())
+        client = chap.GithubClient(NullConfig())
         client.repos = []
         self.assertEquals(client.repo_list(), [])
---
+++ tests/chamberlain/config_test.py
@@ -0,0 +1,38 @@
+import unittest
+
+from chamberlain.config import Config, NullConfig
+
+
+class NullConfigTest(unittest.TestCase):
+    def getattr_returns_self_test(self):
+        cfg = NullConfig()
+        self.assertEqual(cfg, cfg.nested_thing)
+
+    def call_returns_none_test(self):
+        self.assertEqual(NullConfig().thing(), None)
+
+    def nothing_exists_test(self):
+        self.assertEqual(NullConfig().thing.exists(), False)
+
+
+class ConfigTest(unittest.TestCase):
+    def setUp(self):
+        self.cfg = Config({"foo": "bar"})
+
+    def test_existing_k_returns_config(self):
+        self.assertIsInstance(self.cfg.foo, Config)
+
+    def test_non_existing_k_returns_null_config(self):
+        self.assertIsInstance(self.cfg.baz, NullConfig)
+
+    def test_kv_get_on_k_returns_v(self):
+        self.assertEquals(self.cfg.foo(), "bar")
+
+    def test_kv_get_on_nonexistant_k_returns_false(self):
+        self.assertFalse(self.cfg.baz())
+
+    def test_real_kv_exist_returns_true(self):
+        self.assertTrue(self.cfg.foo.exists())
+
+    def test_bad_kv_exist_returns_false(self):
+        self.assertFalse(self.cfg.baz.exists())
