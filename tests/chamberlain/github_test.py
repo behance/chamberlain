eb43da61f7dd34cd641b021010e69fa334d2f844
chr0n1x <heilong24@gmail.com>
2014-11-11 15:13:47 -0500

    github.Client.repo_list: added test for filter

--- tests/chamberlain/github_test.py
+++ tests/chamberlain/github_test.py
@@ -1,11 +1,17 @@
 import unittest
 
-from chamberlain.config import NullConfig
+from chamberlain.config import Config, NullConfig
 from chamberlain.github import Client
 
 
 class ClientTest(unittest.TestCase):
-    def repo_memory_list_test(self):
+    def test_repo_memory_list(self):
         client = Client(NullConfig(), cache_dir="test_dir")
         client.repos = []
         self.assertEquals(client.repo_list(), [])
+
+    def test_filter(self):
+        client = Client(NullConfig(), cache_dir="test_dir")
+        valid_list = [Config({"name": "foobar"}), Config({"name": "foobaz"})]
+        client.repos = valid_list + [Config({"name": "floobygoop"})]
+        self.assertEquals(client.filter_repos(["fooba"]), valid_list)
