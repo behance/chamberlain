19bc2186fa7e8418ec1d45cf771b1b2d8ad8b5c2
chr0n1x <heilong24@gmail.com>
2014-11-12 17:36:19 -0500

    files: write_file(), with write_json_file() as a wrapper

--- chamberlain/files.py
+++ chamberlain/files.py
@@ -1,13 +1,16 @@
 import json
 
 
+def write_file(file_path, contents):
+    with open(file_path, "w") as file_handle:
+        file_handle.write(contents)
+
+
 def load_json_file(file_path):
     with open(file_path, "r") as file_handle:
         content = file_handle.read().replace('\n', '')
-
     return json.loads(content)
 
 
 def write_json_file(file_path, data):
-    with open(file_path, "w") as file_handle:
-        file_handle.write(json.dumps(data))
+    write_file(file_path, json.dumps(data))
