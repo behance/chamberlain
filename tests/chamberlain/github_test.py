9f211fb347701db189b85e2f4764b4ee4d0b9524
chr0n1x <heilong24@gmail.com>
2014-11-14 15:08:07 -0500

    github.Client: update test

--- tests/chamberlain/github_test.py
+++ tests/chamberlain/github_test.py
@@ -12,6 +12,9 @@ class ClientTest(unittest.TestCase):
 
     def test_filter(self):
         client = Client(NullConfig(), cache_dir="test_dir")
-        valid_list = [Config({"name": "foobar"}), Config({"name": "foobaz"})]
-        client.repos = valid_list + [Config({"name": "floobygoop"})]
+        valid_list = [
+            Config({"full_name": "foobar"}),
+            Config({"full_name": "foobaz"})
+        ]
+        client.repos = valid_list + [Config({"full_name": "floobygoop"})]
         self.assertEquals(client.filter_repos(["fooba"]), valid_list)
