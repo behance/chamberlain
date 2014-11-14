37b753e3295ef70b32d5ab31b6de96a6d91f7a97
chr0n1x <heilong24@gmail.com>
2014-11-14 14:12:39 -0500

    jenkins.configuration: correct instance config setup, add __getattr__ in BuilderOption

--- chamberlain/jenkins/configuration.py
+++ chamberlain/jenkins/configuration.py
@@ -6,7 +6,7 @@ from ConfigParser import ConfigParser as JJBConfig
 
 class InstanceConfig(JJBConfig):
     def __init__(self):
-        super(InstanceConfig, self).__init__()
+        JJBConfig.__init__(self)
         self.set_defaults()
 
     def override_defaults(self, opts):
@@ -15,19 +15,24 @@ class InstanceConfig(JJBConfig):
                 self.set(section, k, v)
 
     def set_defaults(self):
+        self.add_section("jenkins")
         self.set("jenkins", "url", "http://localhost:8080/")
         self.set("jenkins", "user", "")
         self.set("jenkins", "password", "")
-        self.set("job_builder", "allow_duplicates", False)
-        self.set("job_builder", "ignore_cache", True)
-        self.set("job_builder", "keep_descriptions", False)
-        self.set("job_builder", "recursive", True)
+        self.add_section("job_builder")
+        self.set("job_builder", "allow_duplicates", "false")
+        self.set("job_builder", "ignore_cache", "true")
+        self.set("job_builder", "keep_descriptions", "false")
+        self.set("job_builder", "recursive", "true")
 
 
 class BuilderOptions(object):
     def __init__(self, template_path, command="update"):
         args = [command, template_path]
         self.opts = jjb.create_parser().parse_args(args)
+
+    def __getattr__(self, name):
+        return self.opts.__getattribute__(name)
 
 
 class ConfigurationRunner:
