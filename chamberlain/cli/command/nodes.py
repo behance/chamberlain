da9c1a1e25ddba5e7377d22fea7eb3900312a3e3
chr0n1x <heilong24@gmail.com>
2015-12-01 14:42:22 -0500

    cli/command/nodes: pep8

--- chamberlain/cli/command/nodes.py
+++ chamberlain/cli/command/nodes.py
@@ -1,8 +1,6 @@
 import chamberlain.application as chap  # lols
 from chamberlain.config import Config
 
-import pprint
-
 
 class Command(object):
     def __init__(self, log, config_file=None):
@@ -44,41 +42,56 @@ class Command(object):
             self.log.info("  - %s: %s" % (key, node[key]))
 
     def attr_wanted(self, node, filters={}):
-        if len(filters) == 0: return True
-        for k,v in filters.iteritems():
-            if k not in node: continue
-            if node[k] == v or v in node[k]: return True
+        if len(filters) == 0:
+            return True
+        for k, v in filters.iteritems():
+            if k not in node:
+                continue
+            if node[k] == v or v in node[k]:
+                return True
         return False
 
     def wanted_node(self, node, filters=[]):
-        if len(filters) == 0: return True
+        if len(filters) == 0:
+            return True
         wrapper = Config(node)
-        if wrapper.displayName() == "master": return False
+        if wrapper.displayName() == "master":
+            return False
         if "offline" in filters or "online" in filters:
-            if wrapper.offline() and "offline" in filters: return True
-            if wrapper.offline() and "online" in filters: return False
+            if wrapper.offline() and "offline" in filters:
+                return True
+            if wrapper.offline() and "online" in filters:
+                return False
         if "idle" in filters or "busy" in filters:
-            if wrapper.idle() and "idle" in filters: return True
-            if wrapper.idle() and "busy" in filters: return False
+            if wrapper.idle() and "idle" in filters:
+                return True
+            if wrapper.idle() and "busy" in filters:
+                return False
         return False
 
     def execute(self, opts):
         conn = self.app.jenkins(opts.instance)
         for node in [conn.get_node_info(n["name"])
-            for n in conn.get_nodes() if n["name"] != "master"]:
-            if not self.wanted_node(node, filters=opts.status_filter): continue
-            attr_filters = {p[0]:p[1] for
-                p in [f.split(":", 1) for f in opts.field_filters]}
-            if not self.attr_wanted(node, filters=attr_filters): continue
+                     for n in conn.get_nodes() if n["name"] != "master"]:
+            if not self.wanted_node(node, filters=opts.status_filter):
+                continue
+            attr_filters = {p[0]: p[1] for
+                            p in [f.split(":", 1) for f in opts.field_filters]}
+            if not self.attr_wanted(node, filters=attr_filters):
+                continue
             self.print_node(node, attrs=attr_filters.keys())
-            if opts.action == "noop": continue
+            if opts.action == "noop":
+                continue
             node_name = node["displayName"]
             try:
                 prog_verb = opts.action[0:-1]
                 self.log.info("%sing %s ..." % (prog_verb, node_name))
-                if opts.action == "disable": conn.disable_node(node_name)
-                if opts.action == "enable": conn.enable_node(node_name)
-                if opts.action == "delete": conn.delete_node(node_name)
+                if opts.action == "disable":
+                    conn.disable_node(node_name)
+                if opts.action == "enable":
+                    conn.enable_node(node_name)
+                if opts.action == "delete":
+                    conn.delete_node(node_name)
             except Exception as e:
                 log_params = (opts.action, node_name, e)
                 self.log.error("Failed to %s %s: %s" % log_params)
